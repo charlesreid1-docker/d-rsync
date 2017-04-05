8b0fe16a676b92b8a29413c8c382888ecc63336b
Charles Reid <charlesreid1@gmail.com>
2017-04-05 06:02:01 +0000

    adding build docker script for d-rsync

---
+++ build_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# 
+# Build the docker container using the Dockerfile
+# 
+# https://charlesreid1.com/wiki/Docker/Dockerfiles
+
+export CONF="rsyncd.conf"
+
+if [ -f $CONF ]; then
+	docker build -t cmr_rsync .
+else
+	echo "Missing config file:"
+	echo "rsyncd.conf"
+	echo ""
+	echo "Add ${CONF} to this directory so it can be copied to /etc/${CONF}"
+fi
